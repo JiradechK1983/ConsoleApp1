6f0cf27badd79d0685c7f1280c799ff99cfc2ed7
JiradechK1983 <jiradech.ke@sbac.ac.th>
2023-06-07 14:44:04 +0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+
+            Console.WriteLine($"Number is {num1}"); // String Interpolation
+            Console.WriteLine("Number is " +  num1); // String Concatetion
+
+            Console.ReadKey();
+        }
+    }
+}
